1b275a1fff92e41399b59aafb496859de758c68a
Colby Rome <cdr013@bucknell.edu>
2018-09-09 18:23:49 -0400

    initial commit

---
+++ device_tracker/quantum_gateway.py
@@ -0,0 +1,79 @@
+import logging
+
+import hashlib
+from http.cookies import SimpleCookie
+import json
+import requests
+import voluptuous as vol
+
+from homeassistant.components.device_tracker import (DOMAIN, PLATFORM_SCHEMA,
+                                                     DeviceScanner)
+from homeassistant.const import (CONF_HOST, CONF_PASSWORD)
+import homeassistant.helpers.config_validation as cv
+
+_LOGGER = logging.getLogger(__name__)
+
+DEFAULT_HOST = 'myfiosgateway.com'
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_HOST, default=DEFAULT_HOST): cv.string
+})
+
+
+def get_scanner(hass, config):
+    scanner = QuantumGatewayDeviceScanner(config)
+
+    return scanner if scanner.success_init else None
+
+
+class QuantumGatewayDeviceScanner(DeviceScanner):
+
+    def __init__(self, config):
+        self.host = 'http://' + config[CONF_HOST]
+        self.password = config[CONF_PASSWORD]
+
+        self.last_results = []
+
+        _LOGGER.info("Initializing")
+
+        self._update_info()
+
+        self.success_init = True
+
+    def scan_devices(self):
+        self._update_info()
+
+        macs = [device['mac'] for device in self.last_results]
+
+        return macs
+
+    def get_device_name(self, device):
+        try:
+            return next(entry['name'] for entry in self.last_results
+                        if entry['mac'] == device)
+        except StopIteration:
+            return None
+
+    def _update_info(self):
+        self.last_results = []
+
+        with requests.Session() as session:
+            getLogin = session.get(self.host + '/api/login')
+            salt = getLogin.json()['passwordSalt']
+
+            encodedPassword = hashlib.sha512()
+            encodedPassword.update((self.password + salt).encode('ascii'))
+
+            payload = json.dumps({"password": encodedPassword.hexdigest()})
+
+            postLogin = session.post(self.host + '/api/login', data=payload)
+            token = SimpleCookie(postLogin.headers.get('set-cookie'))['XSRF-TOKEN'].value
+
+            session.headers.update({'X-XSRF-TOKEN': token})
+
+            devicesRes = session.get(self.host + '/api/devices')
+            devices = json.loads(devicesRes.text)
+
+            session.get(self.host + '/api/logout')
+
+            self.last_results = [{'mac': device['mac'], 'name': device['name']} for device in devices if device['status']]
---
+++ readme.md
@@ -0,0 +1,32 @@
+# Quantum Gateway device_scanner for Home Assistant
+This platform adds support for the Verizon FiOS Quantum Gateway to Home Assistant's presence detection.
+
+This is a work in progress. The end goal is to create a PyPI package and submit a proper pull request to [Home Assistant](https://github.com/home-assistant/home-assistant).
+
+## Instructions
+* Clone or unzip into your custom_components directory.
+* Add to your configuration.yaml:
+```
+device_tracker:
+  - platform: quantum_gateway
+    host: 192.168.1.1
+    password: yourPasswordHere
+```
+
+  Note that providing the `host` is optional. By default, it uses `myfiosgateway.com`. Your password is often found printed on the router.
+
+  My router does not allow me to change the username from `admin` so I did not include it as a parameter.
+
+## Thoughts
+I have only tested on my own Verizon FiOS-provided gateway:
+
+|  |  |
+| --- | --- |
+| UI Version:  | v1.0.294 |
+| Firmware Version: | 02.00.01.08 |
+| Model Name: | FiOS-G1100 |
+| Hardware Version: | 1.03 |
+
+Please open a Github issue if you encounter any problems. Thanks!
+
+![Example FiOS Gateway](https://cdn.arstechnica.net/wp-content/uploads/2016/07/fios-quantum-gateway.png)
