e00df5b478dd9929500244c025c47b758fa1a87c
Sly <git@pplatt.us>
2019-03-03 12:44:53 -0500

    Updated initialization code to default to https
    - added some documentation
    - minor refactor
    
    ref: #7

--- README.md
+++ README.md
@@ -4,31 +4,45 @@ This library allows a Verizon FiOS Quantum Gateway to be queried. It uses the `r
 
 ## Usage
 
-```python
+Please note as of the Firmware version 02.02.00.13 and UI version v1.0.388 https is the only way to get to the admin console. This is using a self signed cert as well. This code now defaults to https and ignores the self signed cert warning. 
+
 from quantum_gateway import QuantumGatewayScanner
 
-# Connect to gateway
+# Connect to gateway via HTTP
+```python
 gateway = QuantumGatewayScanner('192.168.1.1', 'your_password_here')
+``` 
+
+# Connect to gateway via HTTP
+```python 
+gateway = QuantumGatewayScanner('192.168.1.1', 'your_password_here',False)
+``` 
 
 # Ensure successful connection
+```python
 gateway.success_init
-
+```
 # Get list of all connected devices' MAC addresses
+```python
 gateway.scan_devices()
-
+```
 # Get specific device's name
+```python
 gateway.get_device_name('mac address of device here')
 ```
 
-## Thoughts
 
-I have only tested on my own Verizon FiOS-provided gateway:
+
+## Notes
+
+Tested on Verizon FiOS-provided gateway:
 
 |  |  |
 | --- | --- |
-| UI Version:  | v1.0.294 |
-| Firmware Version: | 02.00.01.08 |
+| UI Version:  |  v1.0.388 |
+| Firmware Version: |  02.02.00.13 |
 | Model Name: | FiOS-G1100 |
 | Hardware Version: | 1.03 |
 
 Please open a Github [issue](https://github.com/cisasteelersfan/quantum_gateway/issues) or reply to the Home Assistant forum [post](https://community.home-assistant.io/t/verizon-fios-quantum-gateway-device-tracker-platform/67944) if you encounter any problems. Thanks!
+
--- src/quantum_gateway.py
+++ src/quantum_gateway.py
@@ -7,15 +7,22 @@ import urllib3
 
 TIMEOUT = 5
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
 
 class QuantumGatewayScanner:
-    def __init__(self, host, password):
-        self.host = 'https://' + host
+    def __init__(self, host, password, is_https=True):
+
+        self.verify = False
+
+        if is_https:
+            self.scheme = 'https'
+            urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        else:
+            self.scheme = 'http'
+
+        self.host = self.scheme + '://' + host
         self.password = password
         self.connected_devices = {}
-        self.verify = False
+
         self.session = requests.Session()
 
         self.success_init = self._check_auth()
@@ -30,16 +37,16 @@ class QuantumGatewayScanner:
         return self.connected_devices.get(device)
 
     def _get_connected_devices(self):
-        devices_raw = self.session.get(self.host + '/api/devices', timeout=TIMEOUT, verify=False)
+        devices_raw = self.session.get(self.host + '/api/devices', timeout=TIMEOUT, verify=self.verify)
         devices = json.loads(devices_raw.text)
         self.connected_devices = {device['mac']: device['name'] for device in devices if device['status']}
 
     def _check_auth(self):
-        res = self.session.get(self.host + '/api/devices', timeout=TIMEOUT, verify=False)
+        res = self.session.get(self.host + '/api/devices', timeout=TIMEOUT, verify=self.verify)
         if res.status_code == 200:
             return True
 
-        getLogin = self.session.get(self.host + '/api/login', timeout=TIMEOUT, verify=False)
+        getLogin = self.session.get(self.host + '/api/login', timeout=TIMEOUT, verify=self.verify)
         salt = getLogin.json()['passwordSalt']
 
         encodedPassword = hashlib.sha512()
@@ -47,16 +54,16 @@ class QuantumGatewayScanner:
 
         payload = json.dumps({"password": encodedPassword.hexdigest()})
 
-        postLogin = self.session.post(self.host + '/api/login', data=payload, timeout=TIMEOUT, verify=False)
+        postLogin = self.session.post(self.host + '/api/login', data=payload, timeout=TIMEOUT, verify=self.verify)
         token = SimpleCookie(postLogin.headers.get('set-cookie'))['XSRF-TOKEN'].value
 
         self.session.headers.update({'X-XSRF-TOKEN': token})
 
-        res = self.session.get(self.host + '/api/devices', timeout=TIMEOUT, verify=False)
+        res = self.session.get(self.host + '/api/devices', timeout=TIMEOUT, verify=self.verify)
         if res.status_code == 200:
             return True
         return False
 
     def _log_out(self):
-        self.session.get(self.host + '/api/logout', timeout=TIMEOUT, verify=False)
+        self.session.get(self.host + '/api/logout', timeout=TIMEOUT, verify=self.verify)
         self.session.close()
