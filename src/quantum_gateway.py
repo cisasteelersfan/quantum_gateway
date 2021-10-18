9b35ad3c6f1c33b79c7444df662e8e241b0fde8d
David Stern <vorporeal@gmail.com>
2021-10-17 21:16:22 -0400

    Refactor out a Gateway class in preparation for adding G3100 support.

--- src/quantum_gateway.py
+++ src/quantum_gateway.py
@@ -1,6 +1,8 @@
+from abc import ABC, abstractmethod
 import hashlib
-import json
 from http.cookies import SimpleCookie
+import json
+from typing import Dict, List
 
 import requests
 import urllib3
@@ -8,8 +10,30 @@ import urllib3
 TIMEOUT = 5
 
 
-class QuantumGatewayScanner:
+class Gateway(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.connected_devices = {}
+        self.success_init = False
+
+    @abstractmethod
+    def check_auth(self) -> bool:
+        """Attempts to authenticate with the device.
+        
+        Returns whether or not authentication succeeded.
+        """
+        return NotImplementedError()
+
+    @abstractmethod
+    def get_connected_devices(self) -> Dict[str, str]:
+        """Gets the connected devices as a MAC address -> hostname map."""
+        return NotImplementedError()
+
+
+class Gateway1100(Gateway):
     def __init__(self, host, password, use_https=True):
+        super().__init__()
 
         self.verify = False
         
@@ -21,27 +45,15 @@ class QuantumGatewayScanner:
 
         self.host = self.scheme + '://' + host
         self.password = password
-        self.connected_devices = {}
 
         self.session = requests.Session()
 
-        self.success_init = self._check_auth()
-
-    def scan_devices(self):
-        self.connected_devices = {}
-        if self._check_auth():
-            self._get_connected_devices()
-        return self.connected_devices.keys()
-
-    def get_device_name(self, device):
-        return self.connected_devices.get(device)
-
-    def _get_connected_devices(self):
+    def get_connected_devices(self):
         devices_raw = self.session.get(self.host + '/api/devices', timeout=TIMEOUT, verify=self.verify)
         devices = json.loads(devices_raw.text)
-        self.connected_devices = {device['mac']: device['name'] for device in devices if device['status']}
+        return {device['mac']: device['name'] for device in devices if device['status']}
 
-    def _check_auth(self):
+    def check_auth(self):
         res = self.session.get(self.host + '/api/devices', timeout=TIMEOUT, verify=self.verify)
         if res.status_code == 200:
             return True
@@ -64,6 +76,20 @@ class QuantumGatewayScanner:
             return True
         return False
 
-    def _log_out(self):
-        self.session.get(self.host + '/api/logout', timeout=TIMEOUT, verify=self.verify)
-        self.session.close()
+
+class QuantumGatewayScanner:
+    def __init__(self, host, password, use_https=True):
+        self._gateway = self._get_gateway(host, password, use_https)
+        self.success_init = self._gateway.check_auth()
+
+    def _get_gateway(self, host, password, use_https) -> Gateway:
+        return Gateway1100(host, password, use_https)
+
+    def scan_devices(self) -> List[str]:
+        self.connected_devices = {}
+        if self._gateway.check_auth():
+            self.connected_devices = self._gateway.get_connected_devices()
+        return self.connected_devices.keys()
+
+    def get_device_name(self, device: str) -> str:
+        return self.connected_devices.get(device)
